c98105fdf47c75f88c7f7b9aedf24f1dfeefa776
pikaproject <125982436+pikaproject@users.noreply.github.com>
2023-04-04 23:33:33 +0800

    Update gdriveTools.py

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -640,19 +640,21 @@ class GoogleDriveHelper:
             mime_type = meta.get('mimeType')
             if mime_type == self.__G_DRIVE_DIR_MIME_TYPE:
                 self.__gDrive_directory(meta)
-                msg += f'<b>Name</b>: <code>{name}</code>'
-                msg += f'\n\n<b>Size</b>: {get_readable_file_size(self.__total_bytes)}'
-                msg += '\n\n<b>Type</b>: Folder'
-                msg += f' |<b>SubFolders</b>: {self.__total_folders}'
+                msg += f'<b>_____《🐱 Pik4Bot 🐱》_____</b>'
+                msg += f'\n\n<b>☞ Name</b>: <code>{name}</code>'
+                msg += f'\n<b>☞ Size</b>: {get_readable_file_size(self.__total_bytes)}'
+                msg += f'\n<b>☞ Type</b>: Folder'
+                msg += f'\n<b>☞ SubFolders</b>: {self.__total_folders}'
             else:
-                msg += f'<b>Name</b>: <code>{name}</code>'
+                msg += f'<b>_____《🐱 Pik4Bot 🐱》_____</b>'
+                msg += f'\n\n<b>☞ Name</b>: <code>{name}</code>'
                 if mime_type is None:
                     mime_type = 'File'
                 self.__total_files += 1
                 self.__gDrive_file(meta)
-                msg += f'\n<b>Size</b>: {get_readable_file_size(self.__total_bytes)}'
-                msg += f'\n\n<b>Type</b>: {mime_type}'
-            msg += f' |<b>Files</b>: {self.__total_files}'
+                msg += f'\n<b>☞ Size</b>: {get_readable_file_size(self.__total_bytes)}'
+                msg += f'\n<b>☞ Type</b>: {mime_type}'
+            msg += f'\n<b>☞ Files</b>: {self.__total_files}'
         except Exception as err:
             if isinstance(err, RetryError):
                 LOGGER.info(f"Total Attempts: {err.last_attempt.attempt_number}")
